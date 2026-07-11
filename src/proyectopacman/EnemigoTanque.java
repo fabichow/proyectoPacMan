a4adc9a975b240d266a401883fccb9174e5869bb
LuisPortilla_3 <73078887+fabichow@users.noreply.github.com>
2026-07-11 15:09:49 -0500

    Implementado EnemigoTanque y actualización de ControlEnemigos
    Se implementó la clase EnemigoTanque y se integró en ControlEnemigos

---
+++ src/proyectopacman/EnemigoTanque.java
@@ -0,0 +1,87 @@
+package proyectopacman; 
+
+public class EnemigoTanque { //Enemigo que se mueve lento pero hace 3 de daño. Luis Portilla
+
+    //Atributos
+    int fila;
+    int columna;
+    int daño;
+    boolean activo;
+    int velocidad;
+    String tipo;
+    
+    //Constructor
+    public EnemigoTanque(){
+        tipo = "Tanque";
+        fila = 4;
+        columna = 4;
+        daño = 3;
+        activo = true;
+        velocidad = 1;
+    }
+    public EnemigoTanque(int fila, int columna){
+        tipo = "Tanque";
+        this.fila = fila;
+        this.columna = columna;
+        daño = 3;
+        activo = true;
+        velocidad = 1;
+    }
+    //Metodos
+    public void mover(Jugador j) {
+        actualizarVelocidad(j);
+        for (int i = 0; i < velocidad; i++) {
+            if (fila < j.fila) {
+                fila++;
+            } else if (fila > j.fila) {
+                fila--;
+            } else if (columna < j.columna) {
+                columna++;
+            } else if (columna > j.columna) {
+                columna--;
+            }
+            verificarColision(j);
+        }
+    }
+    public void atacar(Jugador j) {
+        j.recibirDaño(daño);
+        System.out.println("El tanque te atacó!");
+    }
+    public void verificarColision(Jugador j) {
+        if (fila == j.fila && columna == j.columna){
+            atacar(j);
+        }
+    }
+     public int calcularDistancia(Jugador j) {
+        int diferenciaFilas = Math.abs(fila - j.fila);
+        int diferenciaColumnas = Math.abs(columna - j.columna);
+        int distancia = diferenciaFilas + diferenciaColumnas;
+        return distancia;
+    }
+    public void actualizarVelocidad(Jugador j) {
+        int distancia = calcularDistancia(j);
+        if (distancia <= 4) {
+            velocidad = 1;
+        } else if (distancia <= 8) {
+            velocidad = 2;
+        } else {
+            velocidad = 3;
+        }
+    }
+    public boolean estaActivo(){
+        return activo;
+    }
+    public void mostrarEstado() {
+        System.out.println("========Tanque=========");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Fila: " + fila);
+        System.out.println("Columna: " + columna);
+        System.out.println("Dano: " + daño);
+        System.out.println("Velocidad: " + velocidad);
+        if (activo){
+            System.out.println("Estado: Activado");
+        }else{
+            System.out.println("Estado: Desactivado");
+        }
+    }
+}
