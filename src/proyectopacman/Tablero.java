faea075dfaa4c23fd50d319dbbab318fb4be25ce
Ronald Leonel Gutiérrez Neira <leonelgutierrezneira06@gmail.com>
2026-07-10 19:39:28 -0500

    Colisiones
    Agregue colisiones  para que el jugador no salga del tablero y aparte  también le puse colisiones a los enemigos

--- src/proyectopacman/Tablero.java
+++ src/proyectopacman/Tablero.java
@@ -23,24 +23,33 @@ public class Tablero {
             }
         }
     }
-
+    public boolean esMovimientoValido(int fila, int columna) {
+        return fila >= 0 && fila < filas && columna >= 0 && columna < columnas;
+    }
     public void actualizar(Jugador j, EnemigoAcechador e1, EnemigoVelocista e2, Punto[] puntos) {
 
         inicializar();
 
         // puntos
         for (int i = 0; i < puntos.length; i++) {
-            if (!puntos[i].fueRecolectado()) {
+            if (!puntos[i].fueRecolectado()) && esMovimientoValido(puntos[i].fila, puntos[i].columna)){
                 matriz[puntos[i].fila][puntos[i].columna] = '*';
             }
         }
 
-        // jugador
-        matriz[j.fila][j.columna] = 'J';
+        // jugador 
+        if (esMovimientoValido(j.fila, j.columna)) {
+            matriz[j.fila][j.columna] = 'J';
+        }
 
-        // enemigos
-        matriz[e1.fila][e1.columna] = 'A'; // Acechador
-        matriz[e2.fila][e2.columna] = 'V'; // Velocista
+        // Enemigo  Acechador
+        if (esMovimientoValido(e1.fila, e1.columna)) {
+            matriz[e1.fila][e1.columna] = 'A';
+        }
+        // Enemigo Velocista
+       if (esMovimientoValido(e2.fila, e2.columna)) {
+            matriz[e2.fila][e2.columna] = 'V';
+       }
     }
 
     public void mostrar() {
