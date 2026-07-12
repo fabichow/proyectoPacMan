28f863902a0d23c5f223d4ed2fcbeec9d8022a43
Ronald Leonel Gutiérrez Neira <leonelgutierrezneira06@gmail.com>
2026-07-11 23:30:45 -0500

    Update EnemigoVelocista.java
    Agregue el método de control enemigo

--- src/proyectopacman/EnemigoVelocista.java
+++ src/proyectopacman/EnemigoVelocista.java
@@ -1,40 +1,29 @@
 package proyectopacman;
 
 public class EnemigoVelocista { //Enemigo que avanza dos casillas por turno que realiza el jugador. Ronald Gutierrez     
-
-    //Atributos
+    
     int fila;
     int columna;
     int daño;
     boolean activo;
+
     public EnemigoVelocista() {
-        
-        //Constructor
         fila = 6;
         columna = 6;
         daño = 1;
         activo = true;
     }
-    
-    //Metodos
-    public void mover(Jugador j) {
 
-        // se mueve 2 veces por turno (su habilidad) ._.
-         for (int i = 0; i < 2; i++) {
-            if (fila < j.fila) {
-                fila++;
-            }
-            if (fila > j.fila) {
-                fila--;
-            }
-            if (columna < j.columna) {
-                columna++;
-            }
-            if (columna > j.columna) {
-                columna--;
-            }
+    public void mover(Jugador j) {
+        for (int i = 0; i < 2; i++) {
+            if (fila < j.fila) { fila++; }
+            if (fila > j.fila) { fila--; }
+            if (columna < j.columna) { columna++; }
+            if (columna > j.columna) { columna--; }
             verificarColision(j);
         }
+    } 
+
     public void verificarColision(Jugador j) {
         if (fila == j.fila && columna == j.columna) {
             j.recibirDaño(daño);
@@ -48,4 +37,8 @@ public class EnemigoVelocista { //Enemigo que avanza dos casillas por turno que 
         System.out.println("Columna: " + columna);
         System.out.println("Daño: " + daño);
     }
-}   
+
+    public boolean estaActivo() {   //     ControlEnemigos lo llama
+        return activo;              //     comentado, pero si lo activas, lo necesitas
+    }
+}
