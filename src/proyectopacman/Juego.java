8b128eec550ed224b7ea2928a82edc623a0324d5
LuisPortilla_3 <73078887+fabichow@users.noreply.github.com>
2026-07-13 12:52:30 -0500

    Integracion Random y pantalla eleccion de tamaño en Juego

--- src/proyectopacman/Juego.java
+++ src/proyectopacman/Juego.java
@@ -8,17 +8,33 @@ public class Juego {
     EnemigoVelocista velocista;
     EnemigoTanque tanque;
     Tablero tablero;
+    Muro[] muros;
     Punto[] puntos;
     ControlEnemigos controlEnemigos;
     boolean juegoTerminado;
 
     //Contructor
     public Juego(){
+    Scanner lector = new Scanner(System.in);
+    System.out.println("========================================");
+    System.out.println("      CONFIGURACION DEL TABLERO");
+    System.out.println("========================================");
+    System.out.print("Ingrese cantidad de filas: ");
+    int filas = lector.nextInt();
+    System.out.print("Ingrese cantidad de columnas: ");
+    int columnas = lector.nextInt();
+    if (filas < 10 || columnas < 10) {
+        System.out.println("El tamaño minimo es 10x10.");
+        filas = 10;
+        columnas = 10;}
+    muros = generarMuros(filas,columnas);
+    tablero = new Tablero(filas,columnas,muros);
+    gererarPuntos((filas*columnas)/8, filas, columnas);
     jugador = new Jugador();
     acechador = new EnemigoAcechador();
     velocista = new EnemigoVelocista();
     tanque = new EnemigoTanque();
-    tablero = new Tablero(10,10);
+    tablero = new Tablero(filas,columnas,muros);
     puntos = new Punto[3];
     puntos[0] = new Punto(1,1);
     puntos[1] = new Punto(4,4);
@@ -55,6 +71,7 @@ public class Juego {
         } else {
             System.out.println("Movimiento invalido.");
         }
+        verificarPuntos();
         controlEnemigos.moverEnemigos();
         controlEnemigos.verificarColisiones();
         actualizarTablero();
@@ -67,6 +84,23 @@ public class Juego {
             juegoTerminado = true;
         }
     }
+    public void generarPuntos(int cantidad, int filas, int columnas){
+        Random random = new Random();
+        puntos = new Punto[cantidad];
+        for(int = 0; i < cantidad; i++){
+            int fila = random.nextInt(filas);
+            int columna = random.nextInt(columnas);
+            puntos[i] = new Punto(fila,columna);
+        }
+    }
+    public verificarPuntos(){
+        for(int i = 0;  i < puntos.length; i++){
+            if(jugador.fila == puntos[i].fila && jugador.columna == puntos[i].columna && !puntos[i].fueRecolectado()){
+                jugador.recogerPunto(puntos[i]);
+            }
+        }
+    }
+    
     public void mostrarEstado() {
         jugador.mostrarEstado();
         controlEnemigos.mostrarEstadoEnemigos();
